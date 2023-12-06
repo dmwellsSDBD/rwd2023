1e5c12ad10da19d6018041c321dac96477e09030
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-12-06 18:46:35 +0000

    update chapter 12

---
+++ chapter12/your_turn3/tech-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:
+    File Name: script.js
+    Date: 
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.tron').height($(window).height());
+});
